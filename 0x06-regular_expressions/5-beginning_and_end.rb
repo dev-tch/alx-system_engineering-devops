daedf45eeccff3ff85d1df8c28f2d0a6f0ff68a9
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-31 21:40:19 +0000

    regex script5

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+#Find the regular expression that will match the above cases
+pattern=/^h*n$/
+result_match = ""
+
+if ARGV.length >= 1
+  input  = ARGV[0]
+  result_match = input.scan(pattern).join
+end
+puts result_match
