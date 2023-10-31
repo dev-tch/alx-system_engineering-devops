5cfbc681acbe99d450bd3497aa34736ce5c66927
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-31 21:52:25 +0000

    regex script6

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# The regular expression must match a 10 digit phone number
+pattern=/\d{10}/
+result_match = ""
+
+if ARGV.length >= 1
+  input  = ARGV[0]
+  result_match = input.scan(pattern).join
+end
+puts result_match
