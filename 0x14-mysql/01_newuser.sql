b8907f945ed2c3c9a1108c0be7abf80e69e35796
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-23 20:59:34 +0000

    create user and add replication access

---
+++ 0x14-mysql/01_newuser.sql
@@ -0,0 +1,6 @@
+-- reate a MySQL user named holberton_user on both web-01 and web-02 with the host name set 
+-- to localhost and the password projectcorrection280hbtn. 
+-- This will allow us to access the replication status on both servers.
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
