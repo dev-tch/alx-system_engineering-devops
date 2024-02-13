b6187be6f1ff32d1a2b238b6bf77cccbf75904c5
dev-tch <tijeni.chaabeni@gmail.com>
2024-02-13 20:28:47 +0000

    first task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+extract information for api  reddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+              'User-Agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+              }
+    try:
+        res = requests.get(url, headers=headers)
+        json_obj = res.json()
+        item_data = json_obj.get("data", None)
+        if item_data is None:
+            return 0
+        else:
+            item_sub = item_data.get("subscribers")
+            if item_sub is None:
+                return 0
+            else:
+                return item_sub
+    except Exception as e:
+        return 0
