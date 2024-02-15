6fee5634ff1438999db5d5918c9582eafeb665fe
dev-tch <tijeni.chaabeni@gmail.com>
2024-02-15 22:51:36 +0000

    version02 task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""
-extract information for api  reddit
-"""
+"""extract information for api  reddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """return the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-              'User-Agent':
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-              }
+    headers = {'User-Agent': 'Mozilla/5.0'}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code >= 300:
         return 0
