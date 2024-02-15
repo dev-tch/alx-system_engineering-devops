f2ff2088470341eb8f644d78ac21d2dc06a60462
dev-tch <tijeni.chaabeni@gmail.com>
2024-02-15 23:05:07 +0000

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """extract information for api  reddit"""
-import requests
 
 
 def number_of_subscribers(subreddit):
     """return the number of subscribers"""
+    import requests
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'My-User-Agent'}
     res = requests.get(url, headers=headers, allow_redirects=False)
