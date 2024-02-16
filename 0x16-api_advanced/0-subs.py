24f3cdb1480c306fe08926577d09b99168b5d749
dev-tch <tijeni.chaabeni@gmail.com>
2024-02-16 00:10:31 +0000

    task 0 rev11

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,8 @@
 Queries the Reddit API and returns the number of total subscribers for a given
 subreddit.
 """
-import requests
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
@@ -11,9 +12,10 @@ def number_of_subscribers(subreddit):
     Queries the Reddit API and returns the number of total subscribers for a
     given subreddit.
     """
+
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Python/1.0(ALX School 0x16 task 0)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = get(url, headers=headers, allow_redirects=False)
     if response.status_code >= 400:
         return 0
 
