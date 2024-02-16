9156567937c8605764bb668d7e8fdcdbdcf47c49
dev-tch <tijeni.chaabeni@gmail.com>
2024-02-16 00:21:14 +0000

    task0 rev13

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,11 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/1.0(ALX School 0x16 task 0)'}
+    headers = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                    AppleWebKit/537.36 (KHTML, like Gecko)\
+                    Chrome/58.0.3029.110 Safari/537.3'}
     response = get(url, headers=headers, allow_redirects=False)
     if not response.ok:
         return 0
