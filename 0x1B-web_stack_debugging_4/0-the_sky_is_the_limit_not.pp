fed70b225c7817885e85074102f791f0d2c4a1f2
dev-tch <tijeni.chaabeni@gmail.com>
2024-02-22 17:26:09 +0000

    task0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# increase the amount of traffic on ngnix server
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,2 @@
+### project 0x1B. Web stack debugging #4
+
