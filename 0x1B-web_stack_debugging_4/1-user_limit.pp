67494add24183822650b78bd7487363d973f77b3
dev-tch <tijeni.chaabeni@gmail.com>
2024-02-22 17:34:49 +0000

    task2

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message
+exec { 'hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
